9f87d12a9557fab13bcb55118df02fbdf76779c6
dR3am5t0rM <sizhe@hotmail.com>
2016-04-05 16:51:03 +0100

    Got AI working to search depths in trees. Slow and not very smart.

--- src/AI.hs
+++ src/AI.hs
@@ -15,7 +15,8 @@ genAllMoves :: Board -> Col -> [Position]
 genAllMoves b c = [(x,y) | x <- [0..7], y <- [0..7]]
 
 
-genSequence xs ys = sequence [xs,ys]
+generateMoves :: Board -> Col -> [Position]
+generateMoves b c = checkAvailable b (0,0) c
 
 -- Given a function to generate plausible moves (i.e. board positions)
 -- for a player (Col) on a particular board, generate a (potentially)
@@ -52,18 +53,59 @@ buildTree gen b c = let moves = gen b c in -- generated moves
 getBestMove :: Int -- ^ Maximum search depth
             -> GameTree -- ^ Initial game tree
             -> Position
+getBestMove (-1) tree = fst (head (next_moves tree))
 getBestMove 0 (GameTree b c []) = undefined
-getBestMove 0 (GameTree b c ms) = 
-  let next_poss = map fst ms in
-    bestPos (fst next_poss) b c next_poss  
+getBestMove depth (GameTree b c next_moves) = trace ("makeing best move possible") $ 
+    bestPos depth c next_moves 
     where
-      bestPos :: Int -> Board -> Col -> [Position] -> Position
-      bestPos best_pos b c [] = best_pos
-      bestPos s b c (p:ps) = case makeMove b p c of
-          Nothing -> error("Couldn't get best move")
-          Just b' -> bestPos (evaluate b' c) b c ps
+      --snd $ maximum [(value, pos) | value <- evaluate ((makeMove b ) c), pos <- next_poss]
 
-getBestMove depth tree = fst(head (next_moves tree))
+      bestPos :: Int -> Col -> [(Position, GameTree)] -> Position
+      bestPos depth c next_moves = snd $ maximum $ childrenList (depth-1) next_moves c
+
+      --bestPos :: (Position, Int) -> Board -> Col -> [Position] -> (Position, Int)
+      --bestPos best_pos b c [] = best_pos
+
+      --bestPos (pos, value) b c (p:ps) = case makeMove b p c of
+      --    Nothing -> error("Couldn't get best move")
+      --    Just b' -> let x = (evaluate b' c) in 
+      --      case () of 
+      --        _ | x > value -> trace ("found better move" ++ show(x,p) ++ " better than " ++ show(value, pos)) $ bestPos (p, x) b c ps
+      --        _ | otherwise -> bestPos (pos, value) b c ps
+
+--getBestMove depth (GameTree b c ms) =         
+
+childrenList :: Int -> [(Position, GameTree)] -> Col -> [(Int, Position)]
+childrenList _ [] _ = []
+childrenList depth ((p,tree):xs) c = ((evaluateChildren depth tree c), p):(childrenList depth xs c)
+
+--getMax :: Int -> [(Position, GameTree)] -> Col -> Int
+--getMax maxPieces [] c = maxPieces
+--getMax maxPieces ((_, GameTree {game_board = b}):xs) c | e > maxPieces   = getMax e xs
+--                                                       | otherwise = getMax maxPieces xs 
+--                                                       where
+--                                                         e = evaluate b c
+
+
+makeList :: [(Position, GameTree)] -> Col -> [Int]
+makeList [] c = []
+makeList ((_, GameTree {game_board = b}):xs) c = (evaluate b c):(makeList xs c)
+
+getAverage :: [Int] -> Int
+getAverage [] = 0
+getAverage list = sum list `div` length list
+
+evaluateChildren  :: Int -- ^ Depth 
+                  -> GameTree
+                  -> Col
+                  -> Int
+evaluateChildren 0 GameTree {next_moves = ms} c = getAverage $ makeList ms c
+evaluateChildren depth GameTree {next_moves = children} c = getAverage (mapEvaluate depth children c) 
+
+mapEvaluate :: Int -> [(Position, GameTree)] -> Col -> [Int]
+mapEvaluate _ [] _ = []
+mapEvaluate depth ((p, tree):xs) c = trace ("evluating... move " ++ show(p)++ "at depth " ++ show(depth)) $ (evaluateChildren (depth - 1) tree c): (mapEvaluate depth xs c)
+
 
 
 
@@ -79,14 +121,25 @@ updateWorld t World {board = b, turn = c} | gameOver b = let (x,y) = checkScore 
                                                                     | x > y     = error "Black wins!"
                                                                     | otherwise = error "White wins!"
                                                              in result
-                                          | c == Black && validMovesAvailable b c = World b {passes = 0} c
+                                          --Testing AI
+                                          --Black is first move AI
+                                          --White is look at best move for that turn
+                                          | c == Black && validMovesAvailable b c = 
+                                            --let 
+                                            --tree = buildTree genAllMoves b c
+                                            --nextMove = getBestMove (-1) tree in
+                                            --  case makeMove b nextMove c of
+                                            --      Nothing -> error("not possible moves not implemented")
+                                            --      Just b' -> (World (b' {passes = 0}) (other c))
+                                            World b {passes = 0} c
+
                                           -- Need to make sure AI sets passes to 0
                                           | c == White && validMovesAvailable b c = let
                                             tree = buildTree genAllMoves b c
-                                            nextMove = getBestMove 0 tree in
+                                            nextMove = getBestMove 6 tree in
                                               case makeMove b nextMove c of
                                                   Nothing -> error("not possible moves not implemented")
-                                                  Just b' -> (World b' (other c))
+                                                  Just b' -> (World (b' {passes = 0}) (other c))
                                           | otherwise  = trace ("No valid moves for " ++ show c ++ " so their turn is skipped") World (b {passes = (passes b) + 1}) (other c)
 
 
